b27bd581a8d219433b82394821b0ce1af3afb133
Jonathannsh <127175899+Jonathannsh@users.noreply.github.com>
2023-03-14 12:18:53 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="fixed-bottom" style="background-color: cyan">
+  <div class="container">
+    <p class="text-center">UKK RPL 2023 | Jonathan | SMK MAHARDIKA</p>
+  </div>
+
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
